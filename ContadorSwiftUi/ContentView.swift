eeb94eceba794be887c09d9454bb3d47794e7f95
art-Artur Ribeiro <profissional.arturrc@gmail.com>
2025-03-06 20:48:28 -0300

    Contador example

--- ContadorSwiftUi/ContentView.swift
+++ ContadorSwiftUi/ContentView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var count: Int = 0
+    
+    let items = ["item 1","item 2", "item 3", "item 4", "item 5"]
+    
     var body: some View {
+        NavigationView{
+            List(items, id: \.self){
+                item in Text(item)
+            }
+        }
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Contador: \(count)")
+                .font(.title)
+                .padding()
+            
+            Button{
+              count += 1
+            } label: {
+                Text("Incrementar")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
+            }
+            
+            Button{
+              count -= 1
+            } label: {
+                Text("Decrementar")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundStyle(.white)
+                    .cornerRadius(10)
+            }
         }
         .padding()
     }
 }
+    
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}

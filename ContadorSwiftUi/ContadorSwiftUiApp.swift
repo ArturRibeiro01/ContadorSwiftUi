87008f6907912bf86acaefa3268c43133b28a413
art-Artur Ribeiro <profissional.arturrc@gmail.com>
2025-03-06 19:44:13 -0300

    Initial Commit

---
+++ ContadorSwiftUi/ContadorSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContadorSwiftUiApp.swift
+//  ContadorSwiftUi
+//
+//  Created by Artur Mac on 06/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ContadorSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
